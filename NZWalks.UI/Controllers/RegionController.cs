17e13534fd1e30e92943c1282b6a4f2aeb53961d
Singh <akhil-kumar.singh@capgemini.com>
2025-07-04 18:09:02 +0530

    added UI

---
+++ NZWalks.UI/Controllers/RegionController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models.DTO;
+using System.Threading.Tasks;
+
+namespace NZWalks.UI.Controllers
+{
+    public class RegionController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            List<RegionDto> response = new List<RegionDto>();
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+                var httpResponseMessage = await client.GetAsync("https://localhost:7167/api/regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+
+
+            }
+            catch (Exception ex)
+            {
+
+                //Log the exception (ex) here if needed
+            }
+
+            return View(response);
+        }
+    }
+}
