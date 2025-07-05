1723d7a9a1e4a369728635701322989a951d6de1
Singh <akhil-kumar.singh@capgemini.com>
2025-07-05 23:44:14 +0530

    edit crud added

--- NZWalks.UI/Controllers/RegionController.cs
+++ NZWalks.UI/Controllers/RegionController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using NZWalks.UI.Models.DTO;
+using NZWalks.UI.Models;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NZWalks.UI.Controllers
@@ -12,6 +14,7 @@ namespace NZWalks.UI.Controllers
         {
             this.httpClientFactory = httpClientFactory;
         }
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
 
@@ -34,6 +37,53 @@ namespace NZWalks.UI.Controllers
             }
 
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel addRegionViewModel)
+        {
+            var client = httpClientFactory.CreateClient();
+            
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7167/api/regions"),
+                Content = new StringContent(
+                    JsonSerializer.Serialize(addRegionViewModel),
+                    Encoding.UTF8,
+                    "application/json")
+                };
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if(response is not null)
+            {
+                return RedirectToAction("Index" , "Regions");
+            }
+            return View();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            ViewBag.Id = id;
+            var client = httpClientFactory.CreateClient();
+            var response = await client.GetFromJsonAsync<RegionDto>($"https://localhost:7167/api/regions/{id.ToString()}");
+            
+            if (response is not null)
+            {
+                return View(response);
+            }
+            return View(null);
         }
     }
 }
